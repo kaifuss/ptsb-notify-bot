b45647bc3b5f9bf718aad07a1be34ef2d5436e12
Kirill Krasnovskiy <kirusha2435896@gmail.com>
2025-01-02 01:59:04 +0300

    modified:   main.py

--- main.py
+++ main.py
@@ -41,6 +41,7 @@ def process_event(event_data):
 # Обработка подключения клиента
 async def handle_client_connection(client_socket, loop):
     buffer = "" # буфер для обработки каждого отдельного client_socket
+    was_evet_found = False
     try:
         while True:
             # получаем данные с клиентского подключения и наполняем ими буфер
@@ -50,8 +51,10 @@ async def handle_client_connection(client_socket, loop):
             buffer += received_data.decode("utf-8")
 
             # обрабатывем только нужное нам событие. если нужного нет - дропаем
-            if NEEDED_EVENT_DESCRIPTION not in buffer:
+            if (not was_evet_found) and (NEEDED_EVENT_DESCRIPTION not in buffer):
                 buffer = ""
+            else:
+                was_evet_found = True
             
         list_of_lines_in_event = buffer.splitlines()
         for line in list_of_lines_in_event:
